c0bdbbd2a6aa6f93d5e8eb54321bcf6a054b8e1e
Superjom <superjom@gmail.com>
2011-05-21 15:08:37 +0800

    a right wordbar,works well,except a problem with hashIndex

--- src/parser/Thesaurus.pyx
+++ src/parser/Thesaurus.pyx
@@ -10,6 +10,114 @@ Created on May 19, 2011
 
 # *******相关函数可以考虑写为   inline   提供必要接口
 
+#hashIndex 结构
+
+from libc.stdio cimport fopen,fclose,fwrite,FILE,fread
+
+
+cdef struct HI: 
+    int left    #左侧范围
+    int right   #右侧范围
+
+
+cdef class Create_hashIndex:
+    '''
+    建立一级hash参考表
+    使用较复杂的中分法 单独作为一类
+    传入 划分数目：  step
+    结果将会把完整hash划分为step步
+    '''
+    cdef: 
+        object wlist
+        double left     #左侧最小hash
+        double right    #右侧最大hash
+        int step
+        
+
+    def __cinit__(self,left,right,li,step):
+        '''
+        init
+        '''
+        self.wlist=li
+        self.left=left
+        self.right=right
+        self.step=step
+
+    cdef double v(self,double data):
+        '''
+        将元素比较的属性取出
+        '''
+        return data
+
+    def show(self):
+
+        for d in self.wlist:
+            print hash(d),d
+
+    cdef int find(self,double data):
+        '''
+        具体查取值 
+        若存在 返回位置 
+        若不存在 返回   0
+        '''
+        #需要测试 
+        #print 'want to find ',hash(data),data
+        cdef:
+            int l
+            int fir
+            int mid
+            int end
+
+        l=len(self.wlist)
+        dv=self.v(data)     #传入词的hash
+
+        #取得 hash 的一级推荐范围
+        #此处可以进一步推进fir范围 暂时没有必要
+        fir=0
+        end=l-1
+        mid=0
+
+        if l == 0:
+            return 0#空
+
+        while fir<end:
+
+            mid=(fir+ end)/2
+
+            if ( dv > self.v(self.wlist[mid]) ):
+                fir = mid + 1
+
+            elif  dv < self.v(self.wlist[mid]) :
+                end = mid - 1
+
+            else:
+                break
+
+        if fir == end:
+
+            if self.v(self.wlist[fir]) > dv:
+                #假定 在此hash值内
+                return fir-1 
+
+            elif self.v(self.wlist[fir]) < dv:
+                #此处不确定??????
+                return fir
+
+            else:
+                #print 'return fir,mid,end',fir,mid,end
+                return end#需要测试
+                
+        elif fir>end:
+            #此情况为何种情况????????
+            return 0
+
+        else:
+            #print '1return fir,mid,end',fir,mid,end
+            return mid#需要测试
+
+
+
+
 cdef class Create_Thesaurus:
     '''
     新建词库
@@ -20,6 +128,9 @@ cdef class Create_Thesaurus:
         char* ph        #词库的存储目录
         #char* widph     #范围的存储目录 
         object li
+        #hash参考index的范围定义
+        double left
+        double right
 
 
     def __cinit__(self,char* ph):
@@ -56,9 +167,9 @@ cdef class Create_Thesaurus:
         mid=0
         num=hash(word)
 
-        print 'the hash of',word,"is",
-        print num
-        print 'the len of self.li is',l
+        #print 'the hash of',word,"is",
+        #print num
+        #print 'the len of self.li is',l
         
         if l==0:
             print 'the list is empty'
@@ -96,7 +207,7 @@ cdef class Create_Thesaurus:
             
         if first==end:
             if hash(self.li[first])>num:
-                self.li.insert(first,word)+' '
+                self.li.insert(first,word)
                 return False
 
             elif hash(self.li[first])<num:
@@ -161,10 +272,60 @@ cdef class Create_Thesaurus:
         '''
         print 'begin to save width'
         space=' '
-        strr=str(hash( self.li[0] ) ) + space + str(hash( self.li[-1] ) )
+        print 'begin to set hash index in class'
+        #赋值给全局量
+        self.left=hash( self.li[0] ) 
+        self.right=hash( self.li[-1] )
+        strr=str( self.left )+ space + str(self.right )
         print 'the width is',strr
         f=open(widph,'w')
         f.write(strr)
         f.close()
-        
+
+    def create_hash(self,char *ph):
+        '''
+        生成一级索引哈系表
+        需要通过动态分配内存的方式？
+        '''
+        print 'begin create_hash'
+        #分为20个hashindex表
+        cdef: 
+            HI hashIndex[20]
+            int i
+            double minidx
+            long step   #步长 
+            int cur_step
+
+        step=long( (self.right-self.left)/20 )
+        #初始化 Create_hashIndex
+        #产生 hash参考表
+        cdef Create_hashIndex cHashIdx=Create_hashIndex(self.left,self.right,self.li,20)
+        #定义初始 index为1
+        cur_step=0
+
+        print 'get the step:',step
+
+        minidx=self.left
+
+        for i in range(20):
+            #寻找边界
+            minidx+=step*i
+            hashIndex[i].left=cur_step+1
+            hashIndex[i].right=cHashIdx.find(minidx)
+
+        self.save_hash(ph,hashIndex)
+
+    cdef save_hash(self,char *ph,HI *hi):
+        '''
+        将hash参考表进行保存
+        传入 hashindex 数组的指针
+        将hash参考index进行传出
+        '''
+        print 'begin to save hash'
+        cdef FILE *fp=<FILE *>fopen(ph,"wb")
+        fwrite(hi,sizeof(HI),20,fp)
+        fclose(fp)
+        print 'succeed save hash'
+
+
 
