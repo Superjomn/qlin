6eacec53ba99cabbea516649752cfd5428f802aa
Superjom <superjom@gmail.com>
2011-05-25 22:50:15 +0800

    Hit_list 的python数组有问题

--- src/index/indexer.pyx
+++ src/index/indexer.pyx
@@ -1,13 +1,13 @@
-cimport os
+import os
 
-from libc.stdlib cimport malloc,realloc,free
+from libc.stdlib cimport malloc,free
 
 from libc.stdio cimport fopen, fwrite, fread,fclose,FILE
 
-DEF List_init_size 100  #定义List初始化长度
-DEF List_max_size 1000  #定义List最长长度
+DEF List_init_size = 100  #定义List初始化长度
+DEF List_max_size = 1000  #定义List最长长度
 
-DEF List_num 20         #划分 块 数目
+DEF List_num = 20         #划分 块 数目
 
 
 #定义 Hit 结构
@@ -17,7 +17,201 @@ cdef struct Hit:
     short score
     int pos
 
-cdef class List:
+#################### Init_Thesaurus from parser/Thesaurus ##################
+DEF STEP=20
+
+
+cdef struct HI: 
+    int left    #左侧范围
+    int right   #右侧范围
+
+
+      ############## init_hashIndex  from  Thesaurus.pyx  #########
+
+cdef class init_hashIndex:
+    '''
+    init he hash index
+    '''
+    #define the hash index 
+
+    cdef HI hi[STEP]
+
+    def __cinit__(self,char *ph):
+        '''
+        init
+        '''
+        cdef FILE *fp=<FILE *>fopen(ph,"rb")
+        fread(self.hi,sizeof(HI),STEP,fp)
+        fclose(fp)
+
+    def pos(self,double hashvalue):
+        '''
+        pos the word by hashvalue 
+        if the word is beyond hash return -1
+        else return the pos
+        
+        '''
+        cdef int cur=-1
+        
+        if hashvalue>self.hi[0].left:
+            cur+=1
+        else:
+            return cur
+
+        while hashvalue > self.hi[cur].left:
+
+            cur+=1
+
+        return cur
+
+           #################################################
+
+
+#定义 hashIndex 结构
+cdef struct HI: #hashIndex 结构
+    int left    #左侧范围
+    int right   #右侧范围
+    
+
+cdef class Init_thesaurus:
+    '''
+    初始化词库
+    '''
+    #使用动态分配内存方式  
+    #分配词库内存空间
+    cdef char **word_list
+    #一级hash 参考表 初始化
+    cdef init_hashIndex hashIndex
+    #词库长度 由 delloc 调用
+    cdef int length
+
+    def __cinit__(self,char *ph):
+        '''
+        传入词库地址
+        初始化词库
+        '''
+        #一级hash 参考表 初始化
+        self.hashIndex = init_hashIndex("sore/hashIndex.b")
+
+        cdef:
+            int i
+            int l
+
+        f=open(ph)
+        words=f.read()
+        f.close()
+
+        #词的数量 
+        self.length=len(words)
+        cdef char  **li=<char **>malloc( sizeof(char *) * self.length )
+        if li!=NULL:
+            print 'the li is successful'
+            self.word_list=li
+        else:
+            print 'the li is failed'
+
+        #开始对每个词分配内存 
+        #并且分配内存
+        for i,w in enumerate(words):
+            self.word_list[i]=<char *>malloc( sizeof(char) * len(w) )
+            self.word_list[i]=w
+
+    def __dealloc__(self):
+        '''
+        释放c内存空间
+        '''
+        print 'begin to delete all the C spaces'
+
+        cdef char* point
+        cdef int i=0
+
+        #释放每一个词的空间
+        for i in range(self.length):
+            free(self.word_list[i])
+
+        #释放整个词库 pointer 的空间
+        free(self.word_list)
+
+
+    cdef double v(self,data):
+        '''
+        将元素比较的属性取出
+        '''
+        return hash(data)
+
+    def show(self):
+        for d in self.wlist:
+            print hash(d),d
+
+    def find(self,data):
+        '''
+        具体查取值 
+        若存在 返回位置 
+        若不存在 返回   0
+        '''
+        #需要测试 
+        #print 'want to find ',hash(data),data
+        cdef:
+            int l
+            int fir
+            int mid
+            int end
+            int pos
+            HI cur  #范围
+
+        dv=self.v(data)     #传入词的hash
+
+        pos=self.hashIndex( dv )
+
+        if pos!=-1 and pos<STEP:
+            cur=self.hashIndex.hi[pos]
+
+        else:
+            print "the word is not in wordbar or pos wrong"
+            return False
+
+        #取得 hash 的一级推荐范围
+        fir=cur.left
+        end=cur.right
+        mid=fir
+
+        while fir<end:
+            mid=(fir+ end)/2
+            if ( dv > self.v(self.wlist[mid]) ):
+                fir = mid + 1
+            elif  dv < self.v(self.wlist[mid]) :
+                end = mid - 1
+            else:
+                break
+
+        if fir == end:
+            if self.v(self.wlist[fir]) > dv:
+                return 0 
+            elif self.v(self.wlist[fir]) < dv:
+                return 0
+            else:
+                #print 'return fir,mid,end',fir,mid,end
+                return end#需要测试
+                
+        elif fir>end:
+            return 0
+
+        else:
+            #print '1return fir,mid,end',fir,mid,end
+            return mid#需要测试
+
+
+#######################  end Init_thusurus ##################################
+
+
+
+
+
+cdef class Hit_lists:
+    '''
+    hit存储队列
+    每个list对应于一个存储文件
+    '''
     cdef:
         int length
         int top
@@ -43,7 +237,7 @@ cdef class List:
         self.hits[idx].score=score
         self.hits[idx].pos=pos
 
-    cdef bool append(self,wordID,docID,score,pos):
+    cdef bint append(self,wordID,docID,score,pos):
         '''
         向list中添加数据
         如果list溢出 则返回False
@@ -84,17 +278,21 @@ cdef class sorter:
     '''
     最优法排序
     '''
-    cdef object dali
+    #hitlist 的私有对象
+    cdef Hit *dali 
 
-    def __cinit__(self,datalist):
-
+    def __cinit__(self,Hit *datalist):
+        '''
+        初始化 
+        将hit队列传递为self
+        '''
         self.dali=datalist
 
     cdef double gvalue(self,data):
         '''
         取得值
         '''
-        return hash(data[1])
+        return 
 
     def quicksort(self,int p,int q):
 
@@ -144,7 +342,7 @@ cdef class Indexer:
     '''
     #文件目录地址
     cdef char *ph
-    cdef List lists[List_num]
+    cdef Hit_lists hit_list[List_num]
 
     def __cinit__(self,ph):
         '''
@@ -152,6 +350,7 @@ cdef class Indexer:
         ph: wordsplit文件目录地址
         '''
         self.ph=ph
+        self.hit_list=[]
 
     cdef loc_list(self):
         '''
@@ -188,13 +387,13 @@ cdef class Indexer:
                     list_idx=self.loc_list(word)
 
                     if wid:
-                        if self.lists[list_idx].add(word)
+                        if self.hit_list[list_idx].add(word):
                             continue
                         else:
                             #将 list_idx 对应的list写入到文件
                             self.add(list_idx)
                             #将相应list清空
-                            self.lists[list_idx].empty()
+                            self.hit_list[list_idx].empty()
 
     cdef sort(self):
         '''
@@ -207,6 +406,6 @@ cdef class Indexer:
         '''
         cdef FILE *fp=<FILE *>fopen(self.toph,"wb")
         #此处 size 需要???????????????????????????????????????
-        fwrite(hi,sizeof(Hit),List_max_size),fp)
+        fwrite(hi,sizeof(Hit),List_max_size,fp)
         fclose(fp)
 
