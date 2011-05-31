0cb3684174a083d2a2699404c8ecc0611b57364c
Superjom <superjom@gmail.com>
2011-05-31 16:18:01 +0800

    优化代码

--- parser/Thes.pyx
+++ parser/Thes.pyx
@@ -22,12 +22,14 @@ cdef struct HI:
 
 
 cdef class Create_hashIndex:
+
     '''
     建立一级hash参考表
     使用较复杂的中分法 单独作为一类
     传入 划分数目：  step
     结果将会把完整hash划分为step步
     '''
+
     cdef: 
         object wlist
         double left     #左侧最小hash
@@ -37,20 +39,26 @@ cdef class Create_hashIndex:
         
 
     def __cinit__(self,li):
+
         '''
         init
         '''
+
         self.wlist=li
+
 
     def show(self):
 
         for d in self.wlist:
             print hash(d),d
 
+
     cdef int find(self,double data):
+
         '''
         具体查取值 
         '''
+
         #使用更加常规的方式
         cdef:
             int i
@@ -67,9 +75,11 @@ cdef class Create_hashIndex:
 
 
 cdef class init_hashIndex:
+
     '''
     init he hash index
     '''
+
     #define the hash index 
 
     cdef HI hi[STEP]
@@ -80,15 +90,18 @@ cdef class init_hashIndex:
         double right
 
     def __cinit__(self,char *ph,char *wide_ph):
+
         '''
         init
         '''
+
         #取得hash边界
         self.get_wide(wide_ph)
 
         cdef FILE *fp=<FILE *>fopen(ph,"rb")
         fread(self.hi,sizeof(HI),STEP,fp)
         fclose(fp)
+
 
     def show(self):
 
@@ -103,22 +116,26 @@ cdef class init_hashIndex:
             print '-'*50
 
     cdef void get_wide(self,char *ph):
+
         '''
         取得hash的左右边界
         '''
+
         f=open(ph)
         c=f.read()
         f.close()
         self.left = float(c.split()[0])
         self.right = float(c.split()[1])
 
+
     def pos(self,long hashvalue):
+
         '''
         pos the word by hashvalue 
         if the word is beyond hash return -1
         else return the pos
-        
        '''
+
         cdef int cur=-1
         
         cdef double step=<double>( (self.right-self.left)/STEP )
@@ -131,9 +148,11 @@ cdef class init_hashIndex:
 
 
 cdef class Create_Thesaurus:
+
     '''
     新建词库
     '''
+
     #此处字符串传入方式需要确定
 
     cdef: 
@@ -157,21 +176,26 @@ cdef class Create_Thesaurus:
         self.li=[]
     
     def get_words(self):
+
         '''
         返回 词库 字符串
         '''
+
         strr = ''
+
         for i in self.li:
             strr+=i+' '
         return strr
             
 
     def find(self,word):
+
         '''
         在list中查找word
         如果查找到 返回True
         如果没有找到 返回False
         '''
+
         print 'begin find()',word
 
         #定义变量
@@ -199,6 +223,7 @@ cdef class Create_Thesaurus:
             return False
         
         while first<end:
+
             mid=(first+end)/2
 
             if num>hash(self.li[mid]):
@@ -215,12 +240,14 @@ cdef class Create_Thesaurus:
                    
                     if self.li[first]==word:
                         return True
+
                     first-=1
 
                while hash(self.li[end])==num and end<l:
                    
                     if self.li[end]==word:
                         return True 
+
                     end=end+1
 
                self.li.insert(mid+1,word)
@@ -228,7 +255,9 @@ cdef class Create_Thesaurus:
                return False
             
         if first==end:
+
             if hash(self.li[first])>num:
+
                 self.li.insert(first,word)
                 return False
 
@@ -253,17 +282,21 @@ cdef class Create_Thesaurus:
             return True
 
     def show(self):
+
         '''
         展示词库
         '''
+
         for i in self.li:
             print i
 
     cdef char *get_str(self):
+
         '''
         保存时使用字符串直接保存
         将self.li转化为str进行保存
         '''
+
         print 'begin get_str()'
         
         cdef char *str
@@ -275,10 +308,12 @@ cdef class Create_Thesaurus:
         return strr
 
     def save(self):
+
         '''
         保存词库
         格式： 直接采用的字符串空格
         '''
+
         print 'begin to save all the words!'
         print 'the file ph is',self.ph
         print 'str is',self.get_str()
@@ -287,11 +322,14 @@ cdef class Create_Thesaurus:
         f.write( self.get_str() )
         f.close()
 
+
     def save_wide(self,char *widph):
+
         '''
         保存有关  词库中hahs之范围的两
         格式大概为  min max 
         '''
+
         print 'begin to save width'
         space=' '
         print 'begin to set hash index in class'
@@ -299,16 +337,21 @@ cdef class Create_Thesaurus:
         self.left=hash( self.li[0] ) 
         self.right=hash( self.li[-1] )
         strr=str( self.left )+ space + str(self.right )
+
         print 'the width is',strr
+
         f=open(widph,'w')
         f.write(strr)
         f.close()
 
+
     def create_hash(self,char *ph):
+
         '''
         生成一级索引哈系表
         需要通过动态分配内存的方式？
         '''
+
         print 'begin create_hash'
         #分为STEP个hashindex表
         cdef: 
@@ -365,12 +408,15 @@ cdef class Create_Thesaurus:
         self.save_hash(ph,hashIndex)
 
     cdef save_hash(self,char *ph,HI *hi):
+
         '''
         将hash参考表进行保存
         传入 hashindex 数组的指针
         将hash参考index进行传出
         '''
+
         print 'begin to save hash'
+
         cdef FILE *fp=<FILE *>fopen(ph,"wb")
         fwrite(hi,sizeof(HI),STEP,fp)
         fclose(fp)
@@ -378,6 +424,7 @@ cdef class Create_Thesaurus:
         
         ############# debug #######################
         print 'begin to show the hash'
+
         cdef int i
         for i in range(STEP):
             print i
